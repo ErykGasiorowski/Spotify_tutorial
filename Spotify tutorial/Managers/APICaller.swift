2ff394a9ffdcfd0654db87b47b3935b090ac48af
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-04-08 15:05:32 +0200

    wrote another bunch of api calls for browse functionality.

--- Spotify tutorial/Managers/APICaller.swift
+++ Spotify tutorial/Managers/APICaller.swift
@@ -86,10 +86,10 @@ final class APICaller {
         }
     }
     
-    public func getRecommendations(genres: Set<String>, completion: @escaping ((Result<String, Error>) -> Void)) {
+    public func getRecommendations(genres: Set<String>, completion: @escaping ((Result<RecommendationsResponse, Error>) -> Void)) {
         let seeds = genres.joined(separator: ",")
         createRequest(
-            with: URL(string: Constants.baseAPIURL + "/recommendations?seed_genres=\(seeds)"),
+            with: URL(string: Constants.baseAPIURL + "/recommendations?limit=40&seed_genres=\(seeds)"),
             type: .GET
         ) { request in
             let task = URLSession.shared.dataTask(with: request) { data, _, error in
@@ -99,11 +99,12 @@ final class APICaller {
                 }
 
                 do {
-                    let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments) //JSONDecoder().decode(FeaturedPlaylistsResponse.self, from: data)
-                    print(result)
-                    //completion(.success(result))
+                    let result = try JSONDecoder().decode(RecommendationsResponse.self, from: data)
+                    //print(result)
+                    completion(.success(result))
                 }
                 catch {
+                    //print(error)
                     completion(.failure(error))
                 }
             }
---
+++ Spotify tutorial/Models/RecommendationsResponse.swift
@@ -0,0 +1,15 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 08/04/2021.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
+
+
