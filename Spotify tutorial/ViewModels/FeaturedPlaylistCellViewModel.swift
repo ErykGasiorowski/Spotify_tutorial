c247aed881554069c8f533720f23824947682c22
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-04-18 22:10:47 +0200

    finished 2nd and 3rd section of custom cells for browse

---
+++ Spotify tutorial/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 18/04/2021.
+//
+
+import Foundation
+
+struct FeaturedPlaylistCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let creatorName: String
+}
---
+++ Spotify tutorial/ViewModels/RecommendedTrackCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedTrackCellViewModel.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 18/04/2021.
+//
+
+import Foundation
+
+struct RecommendedTrackCellViewModel {
+    let name: String
+    let artistName: String
+    let artworkURL: URL?
+}
