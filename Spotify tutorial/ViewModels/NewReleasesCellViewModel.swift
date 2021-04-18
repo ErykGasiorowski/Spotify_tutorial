5ddf68435e3fb5da5ddf7dc3c4807d8b7516de8b
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-04-18 21:00:02 +0200

    built custom cells for browse - first section

---
+++ Spotify tutorial/ViewModels/NewReleasesCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewReleasesCellViewModel.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 14/04/2021.
+//
+
+import Foundation
+
+struct NewReleasesCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let numberOfTracks: Int
+    let artistName: String
+}
