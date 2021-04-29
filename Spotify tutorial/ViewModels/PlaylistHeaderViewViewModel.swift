e3ba1e9e662d8c26cdc7e0650bf0b0849e12d57c
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-04-29 19:14:15 +0200

    Built playlistvc, added play & share buttons

---
+++ Spotify tutorial/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 29/04/2021.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
