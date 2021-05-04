8aa5df4e5dcbb0c0f45a2644e0467c0c43da4c6a
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-05-04 20:33:26 +0200

    Further development of search ui

---
+++ Spotify tutorial/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  Spotify tutorial
+//
+//  Created by Eryk Gasiorowski on 04/05/2021.
+//
+
+import Foundation
+
+struct CategoryCollectionViewCellViewModel {
+    let title: String
+    let artworkURL: URL?
+}
