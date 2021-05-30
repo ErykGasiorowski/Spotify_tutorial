e147b27116255190bbb04bb3d3177791d737a78e
Eryk Gąsiorowski <erykgasiorowski@hotmail.com>
2021-05-30 16:59:19 +0200

    Sign out option implemented.

--- Spotify tutorial/Managers/AuthManager.swift
+++ Spotify tutorial/Managers/AuthManager.swift
@@ -212,5 +212,19 @@ final class AuthManager {
         }
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
     }
+
+    public func singOut(completion: (Bool) -> Void) {
+        
+        UserDefaults.standard.setValue(nil,
+                                       forKey: "access_token")
+        UserDefaults.standard.setValue(nil,
+                                           forKey: "refresh_token")
+        
+        UserDefaults.standard.setValue(nil,
+                                       forKey: "expirationDate")
+        
+        completion(true)
+    }
     
 }
+
